842d276ae1de53c23447f0fa4cf818bce6a31acd
Panayotis Manganaris <panos.manganaris@gmail.com>
2023-04-20 22:25:53 -0400

    accessor first. import numpy and re.

--- teacherspyt/_team_reflections.py
+++ teacherspyt/_team_reflections.py
@@ -1,42 +1,7 @@
+import numpy as np
 import pandas as pd
 from fuzzywuzzy import process
-
-def normalize_names(ldf, score_threshold = 90)
-  inspected = []
-  for i, row in ldf.iterrows():
-      matches = process.extract(row['name'], ldf['name'].to_dict(), limit=5)
-      idxs = [idx for name, score, idx in matches if score >= score_threshold]
-      # use ~80 for human names, ~90 for emails
-      if i in inspected:
-          continue
-      else:
-          inspected += idxs
-          for idx in idxs:
-              ldf.at[idx, 'name'] = matches[0][0]
-
-def column_aware_aggregator(df):
-      section = df.section.str.split('-').str[0][df['author'] == True] if (
-          True in df['author'].to_list()
-      ) else pd.Series('did not submit (' + df.section.str.split('-').str[0].unique().sum() + ')')
-      section.name = 'section'
-      section = section.reset_index(drop=True)
-      # every student should be an author once, but some don't submit, if so, mark them
-      explanation = pd.Series(" ".join(df.explanation[df.explanation.notna()].to_list())) if (
-          True in df.explanation.notna().to_list()
-      ) else pd.Series([None])
-      explanation.name = 'explanation'
-      explanation = explanation.reset_index(drop=True)
-      # students review eachother and themselves, just collect all reviews from a student's group
-      author = pd.Series(df.author[df.explanation.notna()].any()) if (
-          True in df.explanation.notna().to_list()
-      ) else pd.Series(False)
-      author.name = 'author'
-      author = author.reset_index(drop=True)
-      # if this student was reviewed, it will show up. if they did not write any reviews, author will be false
-      rating = pd.Series(df.rating.mean())
-      rating.name = 'rating'
-      rating = rating.reset_index(drop=True)
-      return pd.concat([section, explanation, author, rating], axis=1)
+import re
 
 @pd.api.extensions.register_dataframe_accessor("pr")
 class PeerReviewAccessor():
@@ -77,3 +42,54 @@ class PeerReviewAccessor():
         sdf['name'] = sdf.name.str.casefold()
 
         return sdf
+
+def normalize_names(ldf, score_threshold = 90):
+    """
+    finds all similar names in ldf and makes them the same.
+
+    Levenshtein similarity score_threshold set at 90 by default, for emails
+    for human names, use ~80
+    """
+    inspected = []
+    for i, row in ldf.iterrows():
+        matches = process.extract(row['name'], ldf['name'].to_dict(), limit=5)
+        idxs = [idx for name, score, idx in matches if score >= score_threshold]
+        if i in inspected:
+            continue
+        else:
+            inspected += idxs
+            for idx in idxs:
+                ldf.at[idx, 'name'] = matches[0][0]
+    return ldf
+
+def student_metric_aggregate(df):
+    breakpoint()
+    section = df.section.str.split('-').str[0][df['author'] == True] if (
+        True in df['author'].to_list()
+    ) else pd.Series(
+        'did not submit (' + df.section.str.split('-').str[0].unique().sum() + ')'
+    )
+    section.name = 'section'
+    section = section.reset_index(drop=True)
+    # every student should be an author once, but some don't submit, if so, mark them
+    explanation = pd.Series(
+        " ".join(df.explanation[df.explanation.notna()].to_list())
+    ) if (
+        True in df.explanation.notna().to_list()
+    ) else pd.Series([None])
+    explanation.name = 'explanation'
+    explanation = explanation.reset_index(drop=True)
+    # students review eachother and themselves, just collect all reviews from a student's group
+    author = pd.Series(
+        df.author[df.explanation.notna()].any()
+    ) if (
+        True in df.explanation.notna().to_list()
+    ) else pd.Series(False)
+    author.name = 'author'
+    author = author.reset_index(drop=True)
+    # if this student was reviewed, it will show up. if they did not write any reviews, author will be false
+    rating = pd.Series(df.rating.mean())
+    rating.name = 'rating'
+    rating = rating.reset_index(drop=True)
+    test = pd.concat([section, explanation, author, rating], axis=1)
+    return test
